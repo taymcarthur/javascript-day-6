2e6da95aedcc787ce74c0e35d350dca1864709bc
Taylor McArthur <taydm.erek@gmail.com>
2017-03-22 16:36:28 -0700

    it works

--- main.js
+++ main.js
@@ -6,14 +6,6 @@ $(document).ready(function(){
 		} else {
 			$("section").removeClass('open')
 			$(this).find("+ section").addClass('open')
-			}
-		})
+		}
 	})
-
-<script
-  src="https://code.jquery.com/jquery-3.1.1.min.js"
-  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-  crossorigin="anonymous"></script>
-
-
 })
